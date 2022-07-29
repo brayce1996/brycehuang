38ef3f954cd3485441e4293a08e4fe1817b49379
Hsin-Yu Huang <brayce1996@gmail.com>
2022-07-29 11:04:45 -0700

    update resume link

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,7 +5,7 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
-        resumeLink: "http://brycehuang.com/static/Hsin-Yu%20Huang's%20Resume.pdf",
+        resumeLink: "https://drive.google.com/file/d/1mxaDue90Xzk4rqMfCSY1tOYWrYxZrPUn/view?usp=sharing",
 
         // for bootstrap grid system
         oneColumnGrid: {
