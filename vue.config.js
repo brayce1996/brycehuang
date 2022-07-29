1a36ffd6bb068663016b4662d56107f4f63ed2a1
Hsin-Yu Huang <brayce1996@gmail.com>
2022-07-29 10:36:30 -0700

    add travis ci

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "static/" : "/",
-    outputDir: process.env.NODE_ENV === "production" ? "dist/static" : "dist",
-    indexPath: process.env.NODE_ENV === "production" ? "../index.html" : "index.html",
     css: {
         loaderOptions: {
             sass: {
